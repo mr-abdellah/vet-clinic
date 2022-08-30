c78e516179575c9108467ffeb06313e6f9840691
mr-abdellah <belkaid.abdulah@gmail.com>
2022-08-30 09:25:30 +0100

    Add spicies and solution

--- queries.sql
+++ queries.sql
@@ -8,3 +8,11 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE round(weight_kg::numeric, 2) >= 10.23 AND round(weight_kg::numeric, 2) <= 17.3;
+
+-- spicies questions 
+select COUNT(*) from animals;
+select COUNT(*) from animals where escape_attempts = 0;
+select avg(weight_kg) from animals;
+select neutered, count(*) from animals group by neutered;
+select min(weight_kg), max(weight_kg) from animals group by species;
+select species, avg(escape_attempts) from animals where date_of_birth between '01/01/1990' and '12/31/2000' group by species;
