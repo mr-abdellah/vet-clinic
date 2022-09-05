05968f2a446457bd226e9645ad2867ef9b1fba2b
Abdellah Belkaid <belkaid.abdulah@gmail.com>
2022-09-05 14:11:04 +0100

    Add performance schema

--- schema.sql
+++ schema.sql
@@ -61,3 +61,13 @@ CREATE TABLE visits (
     vet_id INT REFERENCES vets(id),
     animal_id INT REFERENCES animals(id),
     date_of_visit DATE);
+
+-- Add an email column to your owners table
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+-- Create indexes to improve execution time of queries.
+CREATE INDEX animal_ids ON visits (animal_id);
+
+CREATE INDEX vet_ids ON visits (vet_id);
+
+CREATE INDEX owner_emails ON owners (email);
